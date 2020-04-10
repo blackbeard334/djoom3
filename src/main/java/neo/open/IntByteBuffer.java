f77dda827edffe35599f7faf9472380440981bf4
djidit <djidit@web.de>
2020-04-10 14:15:11 +0200

    IntByteBuffer

--- src/main/java/neo/open/IntByteBuffer.java
+++ src/main/java/neo/open/IntByteBuffer.java
@@ -7,13 +7,32 @@ public class IntByteBuffer {
     private ByteBuffer byteBuffer;
     private IntBuffer intBuffer;
     
+	public void createBuffer(ByteBuffer bb) {
+		this.byteBuffer = bb.duplicate();
+		this.intBuffer = byteBuffer.asIntBuffer();
+	}
+
 	public void createBuffer(int length) {
 		this.byteBuffer = Nio.newByteBuffer(length * 4);
 		this.intBuffer = byteBuffer.asIntBuffer();
 	}
+
+	@Deprecated
+	public void createBuffer(int[] indexes) {
+		this.byteBuffer = Nio.newByteBuffer(indexes.length * 4);
+		this.intBuffer = byteBuffer.asIntBuffer();
+		intBuffer.put(indexes);
+	}
+
 	public ByteBuffer getByteBuffer() {
 		return byteBuffer;
 	}
+
+	@Deprecated
+	public int[] getIntArray() {
+		return intBuffer.array();
+	}
+
 	public IntBuffer getIntBuffer() {
 		return intBuffer;
 	}
