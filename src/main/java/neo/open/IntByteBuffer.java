a19d66458d1eaa05d222ac4b129b792e27875c76
djidit <djidit@web.de>
2020-04-10 15:47:11 +0200

    IntByteBuffer

--- src/main/java/neo/open/IntByteBuffer.java
+++ src/main/java/neo/open/IntByteBuffer.java
@@ -4,9 +4,9 @@ import java.nio.ByteBuffer;
 import java.nio.IntBuffer;
 
 public class IntByteBuffer {
-    private ByteBuffer byteBuffer;
-    private IntBuffer intBuffer;
-    
+	private ByteBuffer byteBuffer;
+	private IntBuffer intBuffer;
+
 	public void createBuffer(ByteBuffer bb) {
 		this.byteBuffer = bb.duplicate();
 		this.intBuffer = byteBuffer.asIntBuffer();
@@ -19,21 +19,55 @@ public class IntByteBuffer {
 
 	@Deprecated
 	public void createBuffer(int[] indexes) {
-		this.byteBuffer = Nio.newByteBuffer(indexes.length * 4);
+		int length = indexes == null ? 0 : indexes.length;
+		this.byteBuffer = Nio.newByteBuffer(length * 4);
 		this.intBuffer = byteBuffer.asIntBuffer();
-		intBuffer.put(indexes);
+		if (length > 0) {
+			intBuffer.put(indexes);
+		}
+	}
+
+	@Deprecated
+	public int[] getAsIntArray() {
+		if (intBuffer.hasArray()) {
+			return intBuffer.array();
+		} else {
+			return new int[intBuffer.capacity()];
+		}
+
 	}
 
 	public ByteBuffer getByteBuffer() {
 		return byteBuffer;
 	}
 
-	@Deprecated
-	public int[] getIntArray() {
-		return intBuffer.array();
-	}
-
 	public IntBuffer getIntBuffer() {
 		return intBuffer;
+	}
+
+	@Deprecated
+	public void putFromIntArray(int[] in, int length) {
+		for (int i = 0; i < length; i++) {
+			intBuffer.put(i, in[i]);
+		}
+	}
+
+	@Deprecated
+	public void putFromIntArray(int[] in, int inIndex, int index, int length) {
+		for (int i = 0; i < length; i++) {
+			intBuffer.put(index + i, in[inIndex + i]);
+		}
+	}
+
+	public void putFromIntBuffer(IntBuffer in, int length) {
+		for (int i = 0; i < length; i++) {
+			intBuffer.put(i, in.get(i));
+		}
+	}
+
+	public void putFromIntBuffer(IntBuffer in, int inIndex, int index, int length) {
+		for (int i = 0; i < length; i++) {
+			intBuffer.put(index + i, in.get(inIndex + i));
+		}
 	}
 }
