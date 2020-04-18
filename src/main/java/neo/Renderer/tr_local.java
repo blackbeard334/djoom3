9edba6f18c61c6ed7f6b2c58974f95d72aadb67c
djidit <djidit@web.de>
2020-04-18 11:18:50 +0200

    Back to Organized imports

--- src/main/java/neo/Renderer/tr_local.java
+++ src/main/java/neo/Renderer/tr_local.java
@@ -754,7 +754,7 @@ public class tr_local {
 
         public renderView_s       renderView;
 //
-        private final FloatBuffer projectionMatrix = Nio.newFloatBuffer(16);
+        private final float[]            projectionMatrix = new float[16];
         public viewEntity_s       worldSpace;
 //
         public idRenderWorldLocal renderWorld;
@@ -856,7 +856,7 @@ public class tr_local {
             }
         }
 
-		public FloatBuffer getProjectionMatrix() {
+		public float[] getProjectionMatrix() {
 			return projectionMatrix;
 		}
     }
